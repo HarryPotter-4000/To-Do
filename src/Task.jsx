ab6526127a21f514ae501fb09aa506243996948d
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2022-01-30 16:37:37 +0200

    useState path

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 
 export default function Task(props) {
   const { name, description, time, isChecked, id, onCheck, onDelete, onEdit } =
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@fortawesome/fontawesome-free/css/all.css";
 import "./style.css";
 import App from "./App";
 
